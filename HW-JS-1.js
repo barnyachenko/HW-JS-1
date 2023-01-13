8aa07e9cc27ce1c39283e4b173d96275cd538790
barnyachenko <veronika.barnyachenko@wesoftyou.com>
2023-01-13 23:22:38 +0100

    added second part of the hw

--- HW-JS-1.js
+++ HW-JS-1.js
@@ -70,17 +70,99 @@
     (не середнє арифметичне значення, а яке з трьох заданих чисел среднє за значенням) використовуючи оператор if-else.
     Використати вкладені оператори if
     Результат виводити в консоль.
+*/ 
 
+    let a = 20;
+    let b = 10;
+    let c = 15;
+
+    if (a > b && a < c) {
+        console.log(a + " - середнє значення")
+    } else if (b > a && b < c) {
+        console.log(b + " - середнє значення")
+    } else if (c > b && c < a) {
+        console.log(c + " - середнє значення")
+    }
+
+/*
 6.  Задано номер дня тижня.
     За заданим номером вивести назву дня тижня використовуючи switch.
     Результат виводити в консоль.
+*/
 
+    let dayOfAWeek = 8;
+
+    if (dayOfAWeek > 7 || dayOfAWeek < 1) {
+        console.log("Such day of a week does not exist")
+    } else {
+        switch (dayOfAWeek) {
+            case 1:
+            console.log ("Monday")
+            break
+
+            case 2:
+            console.log ("Tuesday")
+            break               
+
+            case 3:
+            console.log ("Wednesday")
+            break
+
+            case 4:
+            console.log ("Thursday")
+            break
+
+            case 5:
+            console.log ("Friday")
+            break
+
+            case 6:
+            console.log ("Saturday")
+            break
+
+            case 7:
+            console.log ("Sunday")
+            break
+        }
+    }
+
+/*
 7.  За допомогою switch реалізуйте обчислення виразу, передаючи у switch, як параметр, символ математичної операції.
     Математичні операції для обчислення: "+", "-", "*", "/".
     Результат виводити в консоль.
+*/
 
+    let a1 = 10;
+    let b1 = 5;
+    let mathOperation = "+";
+
+    switch (mathOperation) {
+        case "+":
+        console.log(a1 + b1)
+        break
+
+        case "-":
+        console.log(a1 - b1)
+        break 
+        
+        case "*":
+        console.log(a1 * b1)
+        break
+
+        case "/":
+        console.log(a1 / b1)
+        break
+    }
+
+/*
 8.*  Використовуючи властивості рядків (тип string), та регулярний вираз (regular expression) видалити голосні букви зі слова.
+*/
 
+    let str = "Hello world";    
+    let newStr = str.split('').filter(letter => !['a','e','i','o','u','y'].includes(letter.toLowerCase())).join('');
+    console.log(newStr)
+    
+/*
 9.*  Використовуючи оператор if реалізувати логіку переводу метрів в кілометри,
     так щоб в консоль виводився результат обчислень з правильним закнченням.
     Наприклад: 1000 метрів це 1 кілометр;  32 метри це 0,02 кілометра; 3 кілометри і т.д.
